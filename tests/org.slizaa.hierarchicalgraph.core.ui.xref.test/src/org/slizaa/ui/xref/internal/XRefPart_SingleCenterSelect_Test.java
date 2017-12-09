3948710db8d4c0a907fbc15605baebbeb3e5b978
Gerd Wütherich <gerd@gerd-wuetherich.de>
2017-12-09 22:57:11 +0100

    ~ finally fixed the tests

--- tests/org.slizaa.hierarchicalgraph.core.ui.xref.test/src/org/slizaa/ui/xref/internal/XRefPart_SingleCenterSelect_Test.java
+++ tests/org.slizaa.hierarchicalgraph.core.ui.xref.test/src/org/slizaa/ui/xref/internal/XRefPart_SingleCenterSelect_Test.java
@@ -1,7 +1,6 @@
 package org.slizaa.ui.xref.internal;
 
 import org.eclipse.swtbot.swt.finder.waits.Conditions;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
 import org.junit.Test;
 import org.slizaa.hierarchicalgraph.HGAggregatedDependency;
 import org.slizaa.hierarchicalgraph.HGNode;
@@ -12,57 +11,35 @@ import org.slizaa.hierarchicalgraph.HGNode;
  */
 public class XRefPart_SingleCenterSelect_Test extends AbstractXRefPartTest {
 
-  /**
-   * <p>
-   * </p>
-   * 
-   * @throws InterruptedException
-   */
-  @Test
-  public void test() throws InterruptedException {
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void test() throws InterruptedException {
 
-    //
-    for (int i = 0; i < modules().size(); i++) {
+		//
+		for (int i = 0; i < modules().size(); i++) {
 
-      //
-      HGNode centerModule = (HGNode) modules().get(i);
+			//
+			HGNode centerModule = (HGNode) modules().get(i);
 
-      //
-      System.out.println("Before select centered node...");
-      
-      //
-      centerTree().select(centerRootItem().getNode(getLabel(centerModule)));
-      
-      //
-      System.out.println("Center node selected.");
+			//
+			centerTree().select(centerRootItem().getNode(getLabel(centerModule)));
+			fromRootItem().expand();
+			toRootItem().expand();
 
-      //
-      for (HGAggregatedDependency dependency : centerModule.getIncomingDependenciesFrom(modules())) {
-        
-        
-        
-        
-        String fromLabel = getLabel(dependency.getFrom());
-        
-        System.out.printf("Try to find 'from' node '%s.'\n", fromLabel);
-        
-        for (SWTBotTreeItem item : fromRootItem().getItems()) {
-          System.out.println("ITEM: '" + item.getText() + "'.");
-        }
-        
-        swtbot().waitUntil(Conditions.treeItemHasNode(fromRootItem(), getLabel(dependency.getFrom())));
-        System.out.printf("Found 'from' node '%s.'\n", fromLabel);
-      }
+			//
+			for (HGAggregatedDependency dependency : centerModule.getIncomingDependenciesFrom(modules())) {
+				swtbot().waitUntil(Conditions.treeItemHasNode(fromRootItem(), getLabel(dependency.getFrom())));
+			}
 
-      //
-      for (HGAggregatedDependency dependency : centerModule.getOutgoingDependenciesTo(modules())) {
-        
-        String toLabel = getLabel(dependency.getFrom());
-        
-        System.out.printf("Try to find 'to' node '%s.' \n", toLabel);
-        swtbot().waitUntil(Conditions.treeItemHasNode(toRootItem(), getLabel(dependency.getTo())));
-        System.out.printf("Found 'to' node '%s.'\n", toLabel);
-      }
-    }
-  }
+			//
+			for (HGAggregatedDependency dependency : centerModule.getOutgoingDependenciesTo(modules())) {
+				swtbot().waitUntil(Conditions.treeItemHasNode(toRootItem(), getLabel(dependency.getTo())));
+			}
+		}
+	}
 }
--- tests/org.slizaa.hierarchicalgraph.core.ui.xref.test/src/org/slizaa/ui/xref/internal/XRefPart_SingleCrop_Test.java
+++ tests/org.slizaa.hierarchicalgraph.core.ui.xref.test/src/org/slizaa/ui/xref/internal/XRefPart_SingleCrop_Test.java
@@ -2,7 +2,6 @@ package org.slizaa.ui.xref.internal;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slizaa.hierarchicalgraph.HGAggregatedDependency;
 import org.slizaa.hierarchicalgraph.HGNode;
@@ -13,40 +12,41 @@ import org.slizaa.hierarchicalgraph.HGNode;
  */
 public class XRefPart_SingleCrop_Test extends AbstractXRefPartTest {
 
-  /**
-   * <p>
-   * </p>
-   * 
-   * @throws InterruptedException
-   */
-  @Test
-  @Ignore
-  public void test() throws InterruptedException {
+	/**
+	 * <p>
+	 * </p>
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void test() throws InterruptedException {
 
-    //
-    for (int i = 0; i < modules().size(); i++) {
+		//
+		for (int i = 0; i < modules().size(); i++) {
 
-      //
-      HGNode centerModule = (HGNode) modules().get(i);
-      
-      //
-      centerTree().select(centerRootItem().getNode(getLabel(centerModule)));
-      cropSelectionButton().click();
-      
-      assertThat(centerRootItem().getItems()).hasSize(1);
-      
-      //
-      for (HGAggregatedDependency dependency : centerModule.getIncomingDependenciesFrom(modules())) {
-        fromRootItem().getNode(getLabel(dependency.getFrom()));
-      }
+			//
+			HGNode centerModule = (HGNode) modules().get(i);
 
-      //
-      for (HGAggregatedDependency dependency : centerModule.getOutgoingDependenciesTo(modules())) {
-        toRootItem().getNode(getLabel(dependency.getTo()));
-      }
-      
-      uncropButton().click();
-      assertThat(centerRootItem().getItems()).hasSameSizeAs(modules());
-    }
-  }
+			//
+			centerTree().select(centerRootItem().getNode(getLabel(centerModule)));
+			fromRootItem().expand();
+			toRootItem().expand();
+			cropSelectionButton().click();
+
+			assertThat(centerRootItem().getItems()).hasSize(1);
+
+			//
+			for (HGAggregatedDependency dependency : centerModule.getIncomingDependenciesFrom(modules())) {
+				fromRootItem().getNode(getLabel(dependency.getFrom()));
+			}
+
+			//
+			for (HGAggregatedDependency dependency : centerModule.getOutgoingDependenciesTo(modules())) {
+				toRootItem().getNode(getLabel(dependency.getTo()));
+			}
+
+			uncropButton().click();
+			assertThat(centerRootItem().getItems()).hasSameSizeAs(modules());
+		}
+	}
 }
